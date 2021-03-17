caaeae713cbd814f98c3e0f5fe8ac0e3013feb25
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-03-17 12:51:21 +0500

    Logger service fully integrated

--- packages/lighthouse-crnl/src/browser/lighthouse-crnl-contribution.ts
+++ packages/lighthouse-crnl/src/browser/lighthouse-crnl-contribution.ts
@@ -4,7 +4,8 @@ import {
   CommandRegistry,
   MenuContribution,
   MenuModelRegistry,
-  MessageService,
+  // MessageService,
+  CommandService,
 } from "@theia/core/lib/common";
 import { CommonMenus } from "@theia/core/lib/browser";
 import {
@@ -26,7 +27,8 @@ export const LighthouseCrnlCommand = {
 @injectable()
 export class LighthouseCrnlCommandContribution implements CommandContribution {
   constructor(
-    @inject(MessageService) private readonly messageService: MessageService,
+    // @inject(MessageService) private readonly messageService: MessageService,
+    @inject(CommandService) private readonly commandService: CommandService,
     @inject(TerminalService)
     protected readonly terminalService: TerminalService,
     @inject(EditorManager) private readonly editorManager: EditorManager
@@ -35,74 +37,173 @@ export class LighthouseCrnlCommandContribution implements CommandContribution {
   registerCommands(registry: CommandRegistry): void {
     registry.registerCommand(LighthouseCrnlCommand, {
       execute: () => {
-        let terms: TerminalWidget[] = this.terminalService.all;
+        // let terms: TerminalWidget[] = this.terminalService.all;
+        this.commandService
+          .executeCommand("workbench.action.debug.start")
+          .then(async () => {
+            // Extract log from debugpy log
+            let errLog = await this.extractLog();
+            // Generate execution log
 
-        if (terms.length > 0) {
-          // this.messageService.info('Terminal already created!');
-          this.sendTextToTerminal(terms[0]);
-        } else {
-          this.messageService.info("Creating new terminal");
-          this.terminalService
-            .newTerminal({
-              title: "Lighthouse terminal",
-              shellPath:
-                "c:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
-            })
-            .then((term) => {
-              term.setTitle("Lighthouse Terminal");
-              term.start().then(() => {
-                this.sendTextToTerminal(term);
-              });
-              this.terminalService.open(term);
+            let log = [
+              {
+                executionDate: this.getDate(),
+                log: {
+                  index: this.getIndex(),
+                  executionStatus: errLog ? true : false,
+                  err: {
+                    text: errLog["errText"] ?? "",
+                    position: errLog["errLine"] ?? "",
+                  },
+                },
+              },
+            ];
+
+            // Save log
+            let logPath = `${process.cwd}/log.json`;
+            let logJson = JSON.stringify(log);
+            fs.appendFile(logPath, logJson, () => {
+              console.error(`Could not save log`);
             });
-        }
+          });
       },
     });
   }
 
-  private sendTextToTerminal(term: TerminalWidget): void {
+  /**
+   * Reads the debugpy execution logs and creates a file log.json
+   * in the process root directory.
+   *
+   */
+  private async extractLog(): Promise<any> {
+    var glob = require("glob");
+    let logMap = {};
+
+    await glob(
+      "E:/Foundary/Lighthouse/02-Lighthouse-Experimental/plugins/lighthouse-builtin-ms-python/extension/*.log",
+      function(err: any, files: any) {
+        if (err) {
+          return;
+        } else {
+          let requiredFile = files[3];
+          fs.readFile(requiredFile, "utf-8", (err, data) => {
+            if (err) {
+              console.error("Unable to read the file");
+            } else {
+              try {
+                let breakpoint = data.split("PyDB.do_wait_suspend")[1];
+                // Sending suspend notification.
+                let error = breakpoint.split(
+                  "Sending suspend notification."
+                )[0];
+                let lineByLineErr = error.split("\r\n");
+
+                // Getting line number from error
+                let errNumberLine = lineByLineErr[1].split(" ");
+                let lineNumber = errNumberLine[errNumberLine.length - 1].split(
+                  ")"
+                )[0];
+
+                // Getting error text from the error
+                let errText = lineByLineErr[4].split(", ")[1];
+
+                logMap = {
+                  errLine: lineNumber,
+                  errText: errText,
+                };
+
+                console.log(`Error on line: ${lineNumber}`);
+                console.log(errText);
+              } finally {
+              }
+              files.forEach((file: any) => {
+                fs.unlinkSync(file);
+              });
+            }
+          });
+        }
+      }
+    );
+    return logMap;
+  }
+
+  /**
+   * Returns current date in [day], [month] and [year] format
+   * @returns date: string
+   */
+  private getDate(): string {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+
+    return dd + "/" + mm + "/" + yyyy;
+  }
+
+  /**
+   * Reads the current log file and returns the next log index.
+   * If no log entry is found, returns 1 by default
+   * @returns int
+   */
+  private getIndex(): number {
+    let log = fs.readFileSync(`./log.json`, "utf-8");
+    let logJson = JSON.parse(log);
+
+    if (logJson.length > 0) return logJson.length + 1;
+
+    return 1;
+  }
+
+  protected sendTextToTerminal(term: TerminalWidget): void {
     let editors = this.editorManager.all;
 
     if (editors.length > 0) {
+      // Get current editor
       let currentEditor = editors[0];
-
       currentEditor.editor.focus;
-      currentEditor.editor.onCursorPositionChanged(() => {
-        console.info("User is active");
-      });
 
       let detectedLanguage = currentEditor.editor.document.languageId;
       let currentFilePath = currentEditor.editor.document.uri.split("/");
 
       // term.sendText(`echo Current file language is: ${detectedLanguage}\n`);
       // term.sendText(`echo Current file path: ${currentFilePath}\n`);
-      let fileName = currentFilePath[currentFilePath.length - 1];
+      // let fileName = currentFilePath[currentFilePath.length - 1];
+      let path: String = "";
+      for (let index = 4; index < currentFilePath.length - 1; index++) {
+        const element = currentFilePath[index];
+        path += index == 4 ? element : `/${element}`;
+      }
 
       console.info(`Current file language: ${detectedLanguage}`);
+      console.info(`Current path: ${path}`);
 
-      let logFilePath = process.cwd();
+      // let logFilePath = process.cwd();
       // let command = `python ${fileName} 2>&1 | tee ${logFilePath}\\out_log.txt`;
-      let cmd = `python ${fileName} 2>&1 | ConvertTo-JSON | Out-File ${logFilePath}/log.json -Encoding utf8`;
+
+      let cmd = `python run.py`;
       term.sendText(cmd);
       setTimeout(() => {
         term.sendText("\r");
 
-        setTimeout(this.parseLogFile, 200);
-      }, 500);
+        setTimeout(() => {
+          this.parseLogFile(term);
+        }, 2000);
+      }, 200);
     } else {
       term.sendText("echo No file open\n");
     }
   }
 
-  private parseLogFile(): void {
-    let logFilePath = process.cwd();
-
-    fs.stat(`${logFilePath}\\out_log.txt`, (err, stats) => {
+  private parseLogFile(term: TerminalWidget): void {
+    // let logFilePath = process.cwd();
+    let logFile = "E:/Foundary/Lighthouse/python-test/log.json";
+    fs.stat(logFile, (err, stats) => {
       if (err == null) {
         // file exists
         console.info("Parsing log file");
-        const logFile = fs.readFileSync("out_log.txt");
-        console.info(logFile.toString());
+        const log = fs.readFileSync(logFile, "utf-8");
+        let logJson = JSON.parse(log);
+        term.sendText(`Write-Error ${logJson["err"]}`);
       } else {
         console.error("No file found");
       }
