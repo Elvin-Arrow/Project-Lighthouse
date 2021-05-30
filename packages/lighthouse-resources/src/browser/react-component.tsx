fa278076aeb1b9ef337fb7236074e3dd608f17d7
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-05-30 17:17:20 +0500

    Resource pulling in

--- packages/lighthouse-resources/src/browser/react-component.tsx
+++ packages/lighthouse-resources/src/browser/react-component.tsx
@@ -1,29 +1,34 @@
 import * as React from "react";
 // const Logo = require("../../images/lighthouse.svg") as string;
-const fs = require("fs");
+import fs = require("fs");
 import path = require('path');
 
-export class ReactComponent extends React.Component<{}, { content: string }> {
+export class ReactComponent extends React.Component<{}, { screen: string }> {
 
 	constructor(props: any) {
 		super(props);
 
 		this.state = {
-			content: "<div>Please select a language from the sidebar.</div>"
+			screen: "<div>Please select a language from the sidebar.</div>"
 		};
 	}
 
 	public render(): React.ReactNode {
-		this.getPythonResource().then((resource) => {
-			// TODO Use the resource
-		})
+
+
+		let content;
+		if (this.state.screen == 'python') {
+			content = this.viewPythonResources();
+		} else {
+			content = <div>Please select a language from the sidebar.</div>;
+		}
 		return (
 			<div id="widget-container">
 				<div>
 					<h1>Lighthouse Resources</h1>
 					<h4>Welcome to Lighthouse Resources!</h4>
 				</div>
-				<div className="main-area" dangerouslySetInnerHTML={{ __html: this.state.content }} />
+				<div className="main-area">{content}</div>
 				<div className="sidebar">
 					<div className="links">
 						<h2>Select language</h2>
@@ -39,48 +44,25 @@ export class ReactComponent extends React.Component<{}, { content: string }> {
 		);
 	}
 
-	private languagePython(): void {
-		let pythonContent = '\
-		<div class="resource-block">\
-			<div class="title">if statement</div>\
-			<div class="description">\
-				The program evaluates the test expression and will execute statement(s) only if the test expression is True. If the test expression is False, the statement(s) is not executed.\
-				<code>\
-					if test_expression:\
-					<br />\
-					<span class="indent">statement(s)</span>\
-				</code>\
-			</div>\
-			<div class="readMore"><a href="">Click here to read more...</a></div>\
-		</div>\
-		<div class="resource-block">\
-			<div class="title">for loop</div>\
-			<div class="description">\
-				The val is the variable that takes the value of the item inside the sequence on each iteration. Loop continues until we reach the last item in the sequence. The body of for loop is separated from the rest of the code using indentation.\
-				<code>\
-					for val in sequence:\
-					<br />\
-					<span class="indent">statement(s)</span>\
-				</code>\
-			</div>\
-			<div class="readMore"><a href="">Click here to read more...</a></div>\
-		</div>\
-		<div class="resource-block">\
-			<div class="title">while loop</div>\
-			<div class="description">\
-				In the while loop, test expression is checked first. The body of the loop is entered only if the test_expression evaluates to True. After one iteration, the test expression is checked again. This process continues until the test_expression evaluates to False.\
-				<code>\
-					while test_expression:\
-					<br />\
-					<span class="indent">statement(s)</span>\
-				</code>\
-			</div>\
-			<div class="readMore"><a href="">Click here to read more...</a></div>\
-		</div>';
+	private viewPythonResources(): JSX.Element[] {
+		const resources = this.getPythonResource();
+		let content: JSX.Element[] = [];
 
-		// let temp = this.state.content == "State 1" ? "State 2" : "State 1";
+		resources.forEach((resource) => {
+			content.push(
+				<div className="resource-block">
+					<div className="title">{resource.title}</div>
+					<div className="description">{resource.description}<code>{resource.code}</code></div>
+				</div>
+			);
+		});
+
+		return content;
+	}
+
+	private languagePython(): void {
 		this.setState({
-			content: pythonContent
+			screen: 'python'
 		}, () => { });
 	}
 
@@ -95,15 +77,10 @@ export class ReactComponent extends React.Component<{}, { content: string }> {
 		// }, () => { });
 	}
 
-	private getPythonResource(): Promise<Record<string, any>> {
-		return new Promise<Record<string, any>>((resolve, reject) => {
-			let dataPath = path.join(process.cwd(), 'resources', 'python_resources.json');
-			fs.readFile(dataPath, "utf8", (err: any, jsonString: any) => {
-				if (err) reject(err);
-				const resource = JSON.parse(jsonString);
-				resolve(resource);
-			});
-		});
+	private getPythonResource(): Record<string, any>[] {
+		let dataPath = path.join(process.cwd(), 'resources', 'python_resources.json');
+		const resource = JSON.parse((fs.readFileSync(dataPath, "utf8")));
+		return resource;
 	}
 
 	protected sayHello(): void {
