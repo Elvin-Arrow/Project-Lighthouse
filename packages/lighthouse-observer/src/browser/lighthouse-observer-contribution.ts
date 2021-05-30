f56ca7049299b9e152293c89cbc99709148667ad
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-05-30 12:49:02 +0500

    Observer fully functional

--- packages/lighthouse-observer/src/browser/lighthouse-observer-contribution.ts
+++ packages/lighthouse-observer/src/browser/lighthouse-observer-contribution.ts
@@ -2,6 +2,10 @@ import { injectable, inject } from "inversify";
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from "@theia/core/lib/common";
 import { CommonMenus } from "@theia/core/lib/browser";
 import { EditorManager } from "@theia/editor/lib/browser";
+import Store = require("electron-store");
+import fs = require("fs");
+import path = require('path');
+const homedir = require('os').homedir();
 
 export const LighthouseObserverCommand = {
     id: 'LighthouseObserver.command',
@@ -12,66 +16,97 @@ export const LighthouseObserverCommand = {
 export class LighthouseObserverCommandContribution implements CommandContribution {
     private timeoutId: number;
     private isActive: boolean;
+    private store: Store = new Store();
 
     constructor(
         @inject(MessageService) private readonly messageService: MessageService,
         @inject(EditorManager) private readonly editorManager: EditorManager
     ) {
         this.isActive = true;
-     }
+    }
 
-    startTimer() {
-        // wait 5 seconds before calling goInactive
-        console.info(`Waiting for 5 seconds...`);
+    private startTimer(): void {
+        // wait 5 minutes before calling goInactive
+        console.info(`Waiting for 5 minutes...`);
         this.timeoutId = window.setTimeout(() => {
-            // this.goInactive.bind(this)
             this.messageService.info("User is now inactive");
+
+            // Store the timing
+            this.storeTime();
+
             this.isActive = false;
-        }, 5000);
+            this.store.set('inactive', this.isActive);
+        }, 300000);
     }
 
-    goInactive() {
-        this.messageService.info("User is now inactive");
-    }
-
-    goActive() {
-        // do something
+    private goActive(): void {
         if (!this.isActive) {
-            this.messageService.info("Welcome back");
+            this.messageService.info("We missed you");
         }
 
         this.isActive = true;
+        this.store.set('isActive', this.isActive);
+
         this.startTimer();
     }
 
-    resetTimer(_: any) {
+    private resetTimer(): void {
         console.info(`Resetting timer`);
         try {
             window.clearTimeout(this.timeoutId);
             this.goActive();
-        } catch (_) {
-        }
+        } finally { }
     }
-    
+
     registerCommands(registry: CommandRegistry): void {
         registry.registerCommand(LighthouseObserverCommand, {
             execute: () => {
                 console.info(`Observing the editor...`);
+                let editor = this.editorManager.currentEditor;
 
-                let editors = this.editorManager.all;
-
-                if (editors.length > 0) {
-                    console.info(`Editor handle acquired`);
-
-                    let currentEditor = editors[0];
-
-                    currentEditor.editor.onCursorPositionChanged(this.resetTimer.bind(this));
+                if (editor) {
+                    editor.editor.onDocumentContentChanged((e) => {
+                        console.info(e.contentChanges);
+                        this.resetTimer()
+                    })
                 }
             }
         });
     }
 
-    
+    private storeTime(): void {
+        let statsPath = path.join(homedir, 'lighthouse', `${this.store.get("username")}`, 'assignments', 'stats.json');
+
+        if (fs.existsSync(statsPath)) {
+            let stats = fs.readFileSync(statsPath, 'utf-8')
+
+            // Always true, condition added to enable smart suggestions
+            if (Array.isArray(stats)) {
+                let id = this.store.get('assignmentId');
+
+                if (id) {
+                    console.info(`Updating time stats for assignment Id: ${id}`);
+
+                    stats.forEach(stat => {
+                        if (stat.id == id) {
+                            let time = new Date().getTime();
+                            stat.timespent = time - stat.timespent
+                            console.info(`Updated stat\n${stat}`);
+                        }
+
+                    });
+
+                    fs.writeFile(statsPath, stats, (err) => {
+                        if (err) console.error(`Failed to save the time stats`);
+                    });
+                }
+            }
+        }
+
+
+    }
+
+
 }
 
 @injectable()
