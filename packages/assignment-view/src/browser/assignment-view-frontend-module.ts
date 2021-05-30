470eea837470e2a938af21fb3185a553c3ff9efd
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-05-30 13:00:29 +0500

    Remove menu bar option

--- packages/assignment-view/src/browser/assignment-view-frontend-module.ts
+++ packages/assignment-view/src/browser/assignment-view-frontend-module.ts
@@ -1,15 +1,13 @@
 /**
  * Generated using theia-extension-generator
  */
-import { AssignmentViewCommandContribution, AssignmentViewMenuContribution } from './assignment-view-contribution';
+import { AssignmentViewCommandContribution } from './assignment-view-contribution';
 import {
-    CommandContribution,
-    MenuContribution
+    CommandContribution
 } from "@theia/core/lib/common";
 import { ContainerModule } from "inversify";
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
     bind(CommandContribution).to(AssignmentViewCommandContribution);
-    bind(MenuContribution).to(AssignmentViewMenuContribution);
 });
