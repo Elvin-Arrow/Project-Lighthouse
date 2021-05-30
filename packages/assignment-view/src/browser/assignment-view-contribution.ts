b7472f423acbc258e23f0cb20470988b2f2db804
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-05-30 13:00:19 +0500

    Added activity tracker

--- packages/assignment-view/src/browser/assignment-view-contribution.ts
+++ packages/assignment-view/src/browser/assignment-view-contribution.ts
@@ -1,19 +1,25 @@
 import { injectable, inject } from "inversify";
-import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from "@theia/core/lib/common";
-import { CommonMenus } from "@theia/core/lib/browser";
+import { CommandContribution, CommandRegistry } from "@theia/core/lib/common";
 import { FrontendApplicationStateService } from "@theia/core/lib/browser/frontend-application-state";
 import { WorkspaceService } from "@theia/workspace/lib/browser"; // For handling the workspaces
 import { EditorManager } from "@theia/editor/lib/browser";
 import { CommandService } from "@theia/core";
 import { FileStat } from "@theia/filesystem/lib/common/files";
+import Store = require("electron-store");
+import fs = require("fs");
+import path = require('path');
+const homedir = require('os').homedir();
 
 export const AssignmentViewCommand = {
     id: 'AssignmentView.command',
     label: "Say Hello"
 };
 
+
 @injectable()
 export class AssignmentViewCommandContribution implements CommandContribution {
+    private readonly store: Store = new Store();
+    private assignmentId: number | null = null;
 
     @inject(FrontendApplicationStateService)
     protected readonly stateService: FrontendApplicationStateService;
@@ -63,7 +69,17 @@ export class AssignmentViewCommandContribution implements CommandContribution {
                                 this.editorManager.open(file.resource).then(() => {
                                     if (openReadme)
                                         this.commandService.executeCommand('Markdown-View:command').then(() => {
-                                            // TODO Start observing the editor
+                                            this.commandService.executeCommand('LighthouseObserver.command');
+
+                                            let id = this.getAssignmentId(this.workspaceService.workspace?.name);
+
+                                            console.info(`Assignment ID acquired as: ${id}`);
+
+                                            if (id) {
+                                                this.assignmentId = id;
+                                            }
+                                            // Start noting the time
+                                            this.resetTimer();
                                         });
                                 });
                             } else if (file.name == 'instructions.md') {
@@ -76,16 +92,87 @@ export class AssignmentViewCommandContribution implements CommandContribution {
         });
     }
 
+    private startTimer() {
+        // wait 3 minutes before calling goInactive
+        console.info(`Waiting for 3 minutes...`);
+        window.setTimeout(() => {
+            // this.goInactive.bind(this)
 
-}
+            // Store the timing
+            console.info(`Storing assignment activity`);
+            this.storeTime();
 
-@injectable()
-export class AssignmentViewMenuContribution implements MenuContribution {
+            // Reset timer
+            this.resetTimer();
 
-    registerMenus(menus: MenuModelRegistry): void {
-        menus.registerMenuAction(CommonMenus.EDIT_FIND, {
-            commandId: AssignmentViewCommand.id,
-            label: AssignmentViewCommand.label
-        });
+        }, 180000);
+    }
+
+    private resetTimer() {
+        console.info(`Starting timer`);
+
+        if (this.store.get('isActive', true))
+            this.startTimer();
+        else {
+            // Wait for 1 minute before checking again
+            console.info('User is inactive waiting for 1 minute');
+            setTimeout(() => {
+                this.resetTimer();
+            }, 60000);
+        }
+    }
+
+    private storeTime(): void {
+        let statsPath = path.join(homedir, 'lighthouse', `${this.store.get("username")}`, 'assignments', 'stats.json');
+
+        if (fs.existsSync(statsPath)) {
+            let stats = JSON.parse(fs.readFileSync(statsPath, 'utf-8'));
+
+            console.info('Updating stats with the new time for assignment: ' + this.assignmentId);
+
+            // Always true, condition added to enable smart suggestions
+            if (Array.isArray(stats)) {
+                if (this.assignmentId) {
+
+                    stats.forEach(stat => {
+                        if (stat.id == this.assignmentId) {
+                            let time = new Date().getTime();
+                            stat.timespent = time - stat.timespent
+
+                            console.info(`Updated stat\n${stat}`);
+                        }
+                    })
+                }
+
+                fs.writeFile(statsPath, JSON.stringify(stats), () => {
+                    console.info(`Updated stats`);
+                })
+            }
+        }
+    }
+
+    private getAssignmentId(workspaceName: string | undefined): number | null {
+        let id: number | null = null;
+        if (workspaceName) {
+            workspaceName = workspaceName.split("\\").reverse()[0];
+            console.info(`Looking for assignment: ${workspaceName}`)
+            let assignPath = path.join(homedir, 'lighthouse', `${this.store.get("username")}`, 'assignments', 'assignments.json');
+
+            if (fs.existsSync(assignPath)) {
+                let assignments: Record<string, any>[] = JSON.parse(fs.readFileSync(assignPath, 'utf-8'));
+
+                if (Array.isArray(assignments)) {
+                    assignments.forEach((assignment) => {
+
+                        if (assignment.name == workspaceName) {
+                            id = assignment.id;
+
+                            console.info(`Workspace found, updating ID to: ${id}`);
+                        }
+                    })
+                }
+            }
+        }
+        return id;
     }
 }
